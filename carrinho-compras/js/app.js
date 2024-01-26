0c7793922d1c2cc54a95cb9ae2dbb5e00c1faf2d
Rodolfo Sampaio <rodolfo_1202@hotmail.com>
2024-01-26 18:24:55 -0300

    Roteiro para função adicionar

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,10 @@
+function adicionar() {
+	//recuperar valores: nome do produto, quantidade e valor
+	//calcular o preço: subtotal
+	//adicionar no carrinho
+	//atualizar o valor total
+}
+
+function limpar() {
+
+}
