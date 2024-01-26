05f10b809ea1b6d47769154e2cdb4c35ca1371bb
Rodolfo Sampaio <rodolfo_1202@hotmail.com>
2024-01-26 18:55:32 -0300

    Calculo do Subtotal
    Multiplica o valor do produto pela quantidade

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,21 +1,20 @@
 function adicionar() {
 	//recuperar valores: nome do produto, quantidade e valor
-   let produto = document.getElementById('produto').value;
-   let nomeProduto = produto.split('-')[0]
-   let valorUnitario = produto.split('R$')[1]
+	let produto = document.getElementById('produto').value;
+	let nomeProduto = produto.split('-')[0];
+	let valorUnitario = produto.split('R$')[1];
 	let quantidade = document.getElementById('quantidade');
-   
-   
-   alert(nomeProduto);
+
+	alert(nomeProduto);
 	alert(valorUnitario);
 	alert(quantidade.value);
 
+	let preco = quantidade.value * valorUnitario;
+	alert(preco);
 
 	//calcular o preço: subtotal
 	//adicionar no carrinho
 	//atualizar o valor total
 }
 
-function limpar() {
-
-}
+function limpar() {}
