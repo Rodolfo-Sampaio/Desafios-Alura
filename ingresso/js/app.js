f557159d8618cff23a556ff88e1e66121f0a0e97
Rodolfo Sampaio <rodolfo_1202@hotmail.com>
2024-01-31 16:42:10 -0300

    Verificar se quantidade inserida é um número positivo

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -6,12 +6,16 @@ function comprar() {
 }
 
 function processarCompra(qtd, qtdTipo) {
-	let qtdDisponivel = parseInt(document.getElementById(`qtd-${qtdTipo}`).textContent);
-	if (qtd > qtdDisponivel) {
-		alert(`Quantidade indisponível para tipo ${qtdTipo}`);
+	if (qtd <= 0) {
+		alert('Apenas números positivos são válidos');
 	} else {
-		qtdDisponivel -= qtd;
-		document.getElementById(`qtd-${qtdTipo}`).textContent = qtdDisponivel;
-		alert('Compra realizada com sucesso');
+		let qtdDisponivel = parseInt(document.getElementById(`qtd-${qtdTipo}`).textContent);
+		if (qtd > qtdDisponivel) {
+			alert(`Quantidade indisponível para tipo ${qtdTipo}`);
+		} else {
+			qtdDisponivel -= qtd;
+			document.getElementById(`qtd-${qtdTipo}`).textContent = qtdDisponivel;
+			alert('Compra realizada com sucesso');
+		}
 	}
 }
