91821065e41a220f7b61dde2f6a2c12a8317350c
Rodolfo Sampaio <rodolfo_1202@hotmail.com>
2024-01-31 15:04:23 -0300

    Refatorar evitando repetição e facilitando a leitura

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -1,45 +1,17 @@
 function comprar() {
-   let tipo = document.getElementById('tipo-ingresso');
-   let qtd = parseInt(document.getElementById('qtd').value);
+	let tipo = document.getElementById('tipo-ingresso');
+	let qtd = parseInt(document.getElementById('qtd').value);
 
-   if (tipo.value == 'pista') {
-      comprarPista(qtd);
-   } else if (tipo.value == 'superior') {
-      comprarSuperior(qtd);
-   } else if (tipo.value == 'inferior'){
-      comprarInferior(qtd);
-   }
+	processarCompra(qtd, tipo.value);
 }
 
-function comprarPista(qtd) {
-   let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
-   if (qtd > qtdPista) {
-      alert ('Quantidade indisponível para tipo pista');
-   } else {
-      qtdPista -= qtd;
-      document.getElementById('qtd-pista').textContent = qtdPista;
-      alert ('Compra realizada com sucesso');
-   }
-}
-
-function comprarSuperior(qtd) {
-   let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
-   if (qtd > qtdSuperior) {
-      alert ('Quantidade indisponível para tipo superior');
-   } else {
-      qtdSuperior -= qtd;
-      document.getElementById('qtd-superior').textContent = qtdSuperior;
-      alert ('Compra realizada com sucesso');
-   }
-}
-
-function comprarInferior(qtd) {
-   let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
-   if (qtd > qtdInferior) {
-      alert ('Quantidade indisponível para tipo inferior');
-   } else {
-      qtdInferior -= qtd;
-      document.getElementById('qtd-inferior').textContent = qtdInferior;
-      alert ('Compra realizada com sucesso');
-   }
+function processarCompra(qtd, qtdTipo) {
+	let qtdDisponivel = parseInt(document.getElementById(`qtd-${qtdTipo}`).textContent);
+	if (qtd > qtdDisponivel) {
+		alert(`Quantidade indisponível para tipo ${qtdTipo}`);
+	} else {
+		qtdDisponivel -= qtd;
+		document.getElementById(`qtd-${qtdTipo}`).textContent = qtdDisponivel;
+		alert('Compra realizada com sucesso');
+	}
 }
