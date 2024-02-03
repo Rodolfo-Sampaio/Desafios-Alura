be153c11ef2efb40b651fcf15953f2f5b376a760
Rodolfo Sampaio <rodolfo_1202@hotmail.com>
2024-02-03 10:39:43 -0300

    Adicionado container da calculadora
    Com animação para abertura e fechamento

--- ingresso/js/calculator.js
+++ ingresso/js/calculator.js
@@ -1,10 +1,24 @@
 let calculatorButton = document.getElementById('calculatorButton');
+let calculator = document.getElementById('calculator');
 let overlay = document.getElementById('overlay');
+var closeIcon = document.querySelector('.close-icon');
 
 calculatorButton.addEventListener('click', function (event) {
-	overlay.classList.add('open'); 
+	overlay.classList.add('overlay-open');
+	calculator.classList.add('calculator-container-open');
+	closeIcon.classList.add('close-icon-open');
 });
 
 overlay.addEventListener('click', function (event) {
-	overlay.classList.remove('open');
+	if (event.target === overlay) {
+		overlay.classList.remove('overlay-open');
+		calculator.classList.remove('calculator-container-open');
+		closeIcon.classList.remove('close-icon-open');
+	}
+});
+
+closeIcon.addEventListener('click', function () {
+	overlay.classList.remove('overlay-open');
+	calculator.classList.remove('calculator-container-open');
+	closeIcon.classList.remove('close-icon-open');
 });
