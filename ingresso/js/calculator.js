ce8b2995f8aa90677bb14d8e13d2e2b9bd85ca8d
Rodolfo Sampaio <rodolfo_1202@hotmail.com>
2024-02-03 13:31:04 -0300

    Adicionado ícones das operações

--- ingresso/js/calculator.js
+++ ingresso/js/calculator.js
@@ -2,8 +2,11 @@ let calculatorButton = document.getElementById('calculatorButton');
 let calculator = document.getElementById('calculator');
 let overlay = document.getElementById('overlay');
 var closeIcon = document.querySelector('.close-icon');
+var calculatorContainer = document.querySelector('.calculator-container');
 
 calculatorButton.addEventListener('click', function (event) {
+	overlay.style.display = 'block';
+	calculatorContainer.style.display = 'block';
 	overlay.classList.add('overlay-open');
 	calculator.classList.add('calculator-container-open');
 	closeIcon.classList.add('close-icon-open');
