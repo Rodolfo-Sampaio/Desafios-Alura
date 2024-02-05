f96c227d60d0e338aa3a7b131c991f41de469871
Rodolfo Sampaio <rodolfo_1202@hotmail.com>
2024-02-04 23:18:11 -0300

    Refatorar evitando repetições

--- ingresso/js/calculator.js
+++ ingresso/js/calculator.js
@@ -1,28 +1,31 @@
 let calculatorButton = document.getElementById('calculatorButton');
 let calculator = document.getElementById('calculator');
 let overlay = document.getElementById('overlay');
-var closeIcon = document.querySelector('.close-icon');
-var calculatorContainer = document.querySelector('.calculator-container');
+let closeIcon = document.querySelector('.close-icon');
+let calculatorContainer = document.querySelector('.calculator-container');
 
-calculatorButton.addEventListener('click', function (event) {
+function toggleCalculatorDisplay() {
 	overlay.style.display = 'block';
 	calculatorContainer.style.display = 'flex';
 	overlay.classList.add('overlay-open');
 	calculator.classList.add('calculator-container-open');
 	closeIcon.classList.add('close-icon-open');
-});
+}
+
+function closeCalculatorDisplay() {
+	overlay.classList.remove('overlay-open');
+	calculator.classList.remove('calculator-container-open');
+}
+
+calculatorButton.addEventListener('click', toggleCalculatorDisplay);
 
 overlay.addEventListener('click', function (event) {
 	if (event.target === overlay) {
-		overlay.classList.remove('overlay-open');
-		calculator.classList.remove('calculator-container-open');
+		closeCalculatorDisplay();
 	}
 });
 
-closeIcon.addEventListener('click', function () {
-	overlay.classList.remove('overlay-open');
-	calculator.classList.remove('calculator-container-open');
-});
+closeIcon.addEventListener('click', closeCalculatorDisplay);
 
 let chosenOperation = document.getElementById('chosenOperation');
 
@@ -30,15 +33,15 @@ function calculateAndSetOperation(operation, iconClass) {
 	chosenOperation.classList.add(operation);
 	let result = document.getElementById('result');
 	chosenOperation.innerHTML = /*html*/ `
-		 <div class="operation-icon ${operation}-active">
-			  <i class="fas ${iconClass}"></i>
-		 </div>
-	`;
+    <div class="operation-icon ${operation}-active">
+      <i class="fas ${iconClass}"></i>
+    </div>
+  `;
 
 	let a = parseInt(document.getElementById('inputA').value);
 	let b = parseInt(document.getElementById('inputB').value);
 	let operationName = '';
-	let calculationResult = calculator(operation, a, b);
+	let calculationResult;
 
 	function add(a, b) {
 		return a + b;
@@ -56,9 +59,10 @@ function calculateAndSetOperation(operation, iconClass) {
 		if (b !== 0) {
 			return a / b;
 		} else {
-			return 'Erro: divisão por zero.';
+			throw new Error('Erro: divisão por zero.');
 		}
 	}
+
 	function calculator(operation, a, b) {
 		switch (operation) {
 			case 'add':
@@ -74,33 +78,26 @@ function calculateAndSetOperation(operation, iconClass) {
 				operationName = 'divisão';
 				return divide(a, b);
 			default:
-				return 'Operação inválida.';
+				throw new Error('Operação inválida.');
 		}
 	}
 
-	if (calculationResult !== 'Erro: divisão por zero.') {
+	try {
+		calculationResult = calculator(operation, a, b);
 		result.innerText = `A ${operationName} é ${calculationResult}`;
-	} else {
-		result.innerText = calculationResult;
+	} catch (error) {
+		result.innerText = error.message;
 	}
 }
 
-let addIcon = document.querySelector('.add');
-addIcon.addEventListener('click', function (event) {
-	calculateAndSetOperation('add', 'fa-plus');
-});
+function setupOperationButton(operation, iconClass) {
+	let operationIcon = document.querySelector(`.${operation}`);
+	operationIcon.addEventListener('click', function (event) {
+		calculateAndSetOperation(operation, iconClass);
+	});
+}
 
-let subtractIcon = document.querySelector('.subtract');
-subtractIcon.addEventListener('click', function (event) {
-	calculateAndSetOperation('subtract', 'fa-minus');
-});
-
-let multiplyIcon = document.querySelector('.multiply');
-multiplyIcon.addEventListener('click', function (event) {
-	calculateAndSetOperation('multiply', 'fa-times');
-});
-
-let divideIcon = document.querySelector('.divide');
-divideIcon.addEventListener('click', function (event) {
-	calculateAndSetOperation('divide', 'fa-divide');
-});
+setupOperationButton('add', 'fa-plus');
+setupOperationButton('subtract', 'fa-minus');
+setupOperationButton('multiply', 'fa-times');
+setupOperationButton('divide', 'fa-divide');
