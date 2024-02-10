b95510ff4b984959580bbd4d1d5a416df16d28cb
Rodolfo Sampaio <rodolfo_1202@hotmail.com>
2024-02-10 11:48:15 -0300

    Embaralhando o array de amigos

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -1,12 +1,29 @@
+let amigos = [];
+
 function adicionar() {
 	let amigo = document.getElementById('nome-amigo');
 	let listaDeAmigos = document.getElementById('lista-amigos');
+
+	amigos.push(amigo.value);
 
 	if (listaDeAmigos.textContent == '') {
 		listaDeAmigos.textContent = amigo.value;
 	} else {
 		listaDeAmigos.textContent += ', ' + amigo.value;
 	}
-   
+
 	amigo.value = '';
+}
+
+function sortear() {
+	embaralha(amigos);
+   alert(amigos)
+}
+
+function embaralha(lista) {
+	for (let indice = lista.length; indice; indice--) {
+		const indiceAleatorio = Math.floor(Math.random() * indice);
+
+		[lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+	}
 }
