034110571593469234f39830a4190c3015696d50
Rodolfo Sampaio <rodolfo_1202@hotmail.com>
2024-02-10 16:03:55 -0300

    Validando o nome do amigo

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -2,6 +2,12 @@ let amigos = [];
 
 function adicionar() {
 	let amigo = document.getElementById('nome-amigo');
+
+	if (amigo.value == '') {
+		alert('Digite um nome');
+		return;
+	}
+
 	let listaDeAmigos = document.getElementById('lista-amigos');
 
 	amigos.push(amigo.value);
@@ -18,7 +24,7 @@ function adicionar() {
 function sortear() {
 	embaralha(amigos);
 	let listaSorteio = document.getElementById('lista-sorteio');
-   
+
 	listaSorteio.innerHTML = '';
 
 	for (let i = 0; i < amigos.length; i++) {
